8111ed6fca2b1e1796f53fe7fc480ae294eb6bb3
Cho Zin Thet <chozinthet20602@gmail.com>
2021-01-17 16:21:21 +0630

    finish v-0.1 with responsive

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      'black': '#000',
+      'white': '#fff',
+      'gray-light': '#dee1ec',
+      'gray-dark': '#3e3636',
+      'red': '#d72323',
+      'blue': '#006AB2',
+      'red-light': '#CC4531',
+      'blue-light': '#5F96FF',
+      'green-light': '#8EB21F',
+      'green': '#6b8813'  
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
